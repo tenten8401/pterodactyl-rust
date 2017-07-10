188b8611d43dd0ccf18a64fc18351dca74eddc76
Unknown <isaacalich8401@gmail.com>
2017-07-09 22:11:53 -0400

    Update for OxideMod

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,15 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+if [ -f OXIDE_FLAG ]; then
+    echo "Updating OxideMod..."
+    curl "https://dl.bintray.com/oxidemod/builds/Oxide-Rust.zip" > oxide.zip
+    unzip oxide.zip
+    rm oxide.zip
+fi
+
+# Fix for Rust not starting
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+
 # Run the Server
 node /wrapper.js "${MODIFIED_STARTUP}"
